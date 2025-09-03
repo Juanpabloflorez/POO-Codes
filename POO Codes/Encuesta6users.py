c7efc1e15809d7de42fe66a82f0260d367b36b53
Juanpabloflorez <juflorezru@unal.edu.co>
2025-09-03 11:19:41 -0500

    Update Encuesta6users.py

--- POO Codes/Encuesta6users.py
+++ POO Codes/Encuesta6users.py
@@ -1,2 +1,24 @@
 #Autor: Juan Pablo Florez Rubio
 
+RespuestasNombre=[None]*6
+RespuestasCarrera=[None]*6
+RespuestasIdea=[None]*6
+
+
+for i in range(6):
+    print("Persona ", i+1)
+    RespuestasNombre[i] = str(input("Ingrese su nombre: "))
+    RespuestasCarrera[i] = str(input("Ingrese su carrera: "))
+    RespuestasIdea[i] = str(input("Ingrese su idea de proyecto: "))
+    print("\n")
+
+print("RESULTADOS")
+print("\n")
+for i in range(6):
+    print("Persona ", i+1)
+    print("Nombre: ", RespuestasNombre[i])
+    print("Carrera: ", RespuestasCarrera[i])
+    print("Idea de proyecto: ", RespuestasIdea[i])
+    print("\n")
+
+
